596ad8d0ddcab789abc55c1331326d9e8ffda106
Blondberg <iamblondberg@gmail.com>
2023-12-15 23:56:25 +0100

    Moved board to a class. Updated to check for invalid inputs

--- connect4.py
+++ connect4.py
@@ -3,178 +3,175 @@ import random
 import copy
 
 
-def setup_board(cols, rows):
-    """Create a new board based on set number of columns and rows
+class Board(object):
+    def __init__(self, board, last_move=[None, None]) -> None:
+        self.board = board
+        self.last_move = last_move
 
-    Args:
-        cols (int): number of columns
-        rows (int): number of rows
+    def is_valid_col(self, col):
+        """Check if top location of row is empty
 
-    Returns:
-        np.ndarray: array of size (rows, cols) filled with zeroes
-    """
-    return np.zeros((rows, cols))
+        Args:
+            board (np.ndarray): a game board
+            col (int): zero-indexed column to check
 
+        Returns:
+            bool: True if row is empy, False otherwise
+        """
+        try:
+            return self.board[len(self.board) - 1][col] == 0
+        except IndexError as e:
+            return False
 
-def is_valid_col(board, col):
-    """Check if top location of row is empty
+    def get_valid_row(self, col):
+        """Get the next valid row in a chosen column (is 0)
 
-    Args:
-        board (np.ndarray): a game board
-        col (int): zero-indexed column to check
+        Args:
+            board (np.ndarray): a game board
+            col (int): column to check
 
-    Returns:
-        bool: True if row is empy, False otherwise
-    """
-    return board[len(board) - 1][col] == 0
+        Returns:
+            int: index of valid row, -1 if nothing is found
+        """
+        for i in range(len(self.board)):
+            if self.board[i][col] == 0:
+                return i
 
+        return -1
 
-def get_valid_cols(board):
-    """Get a list of the current valid columns to drop in
+    def legal_moves(self):
+        """Get a list of the current valid columns to drop in
 
-    Args:
-        board (np.ndarray): a game board
+        Returns:
+            list: list of valid column indices
+        """
+        return [i for i in range(len(self.board[0])) if self.is_valid_col(i)]
 
-    Returns:
-        list: list of valid column indices
-    """
-    return [i for i in range(len(board[0])) if is_valid_col(board, i)]
+    def drop_token(self, row, col, token):
+        """Sets the index at (row, col) in board to set token value
 
+        Args:
+            row (int): row to drop in
+            col (int): col to drop in
+            token (int): value of token
 
-def get_valid_row(board, col):
-    """Get the next valid row in a chosen column (is 0)
+        Returns:
+            Board: a copy of the board with the next move set
+        """
+        temp_board = copy.deepcopy(self)
+        temp_board.board[row][col] = token
+        return temp_board
 
-    Args:
-        board (np.ndarray): a game board
-        col (int): column to check
+    def print(self):
+        """Prints the board to the screen
 
-    Returns:
-        int: index of valid row
-    """
-    for i in range(len(board)):
-        if board[i][col] == 0:
-            return i
+        Args:
+            board (np.ndarray): a game board
+        """
+        print("#############")
+        for row in np.flip(self.board, 0):
+            print(row)
+        print("#############")
 
+    def check_for_winner(self):
+        """Checks if there is a winner on the board by looking for 4-in-a-row of any character except 0
 
-def drop_token(board, row, col, token):
-    """Sets the index at (row, col) in board to set token value
+        Args:
+            board (np.ndarray): a game board
 
-    Args:
-        board (np.ndarray): a game board
-        row (int): row to drop in
-        col (int): col to drop in
-        token (int): value of token
+        Returns:
+            bool: Winner character, empty if no winner
+        """
+        num_cols = len(self.board[0])
+        num_rows = len(self.board)
+        # check rows
+        for row in self.board:
+            for col in range(len(row) - 3):
+                if row[col] == row[col + 1] == row[col + 2] == row[col + 3] != 0:
+                    return row[col]
+        # check columns
+        for col in range(num_cols):
+            for row in range(len(self.board) - 3):
+                if (
+                    self.board[row][col]
+                    == self.board[row + 1][col]
+                    == self.board[row + 2][col]
+                    == self.board[row + 3][col]
+                    != 0
+                ):
+                    return self.board[row][col]
 
-    Returns:
-        np.ndarray: copy of board with the new change
-    """
-    temp_board = copy.deepcopy(board)
-    temp_board[row][col] = token
-    return temp_board
+        # Check upward diagonals
+        for row in range(num_rows - 3):
+            for col in range(num_cols - 3):
+                if (
+                    self.board[row][col]
+                    == self.board[row + 1][col + 1]
+                    == self.board[row + 2][col + 2]
+                    == self.board[row + 3][col + 3]
+                    != 0
+                ):
+                    return self.board[row][col]
 
+        # Check downward diagonals
+        for row in range(3, num_rows):
+            for col in range(num_cols - 3):
+                if (
+                    self.board[row][col]
+                    == self.board[row - 1][col + 1]
+                    == self.board[row - 2][col + 2]
+                    == self.board[row - 3][col + 3]
+                    != 0
+                ):
+                    return self.board[row][col]
 
-def print_board(board):
-    """Prints the board to the screen
+        return ""
 
-    Args:
-        board (np.ndarray): a game board
-    """
-    print("#############")
-    for row in np.flip(board, 0):
-        print(row)
-    print("#############")
+    def play_game(self):
+        turn_counter = 0
+        game_over = False
+        player_turn = True
 
-
-def check_for_winner(board):
-    """Checks if there is a winner on the board by looking for 4-in-a-row of any character except 0
-
-    Args:
-        board (np.ndarray): a game board
-
-    Returns:
-        bool: True if a winner is found, otherwise False
-    """
-    num_cols = len(board[0])
-    num_rows = len(board)
-    # check rows
-    for row in board:
-        for col in range(len(row) - 3):
-            if row[col] == row[col + 1] == row[col + 2] == row[col + 3] != 0:
-                return row[col]
-    # check columns
-    for col in range(num_cols):
-        for row in range(len(board) - 3):
-            if (
-                board[row][col]
-                == board[row + 1][col]
-                == board[row + 2][col]
-                == board[row + 3][col]
-                != 0
-            ):
-                return True
-
-    # Check upward diagonals
-    for row in range(num_rows - 3):
-        for col in range(num_cols - 3):
-            if (
-                board[row][col]
-                == board[row + 1][col + 1]
-                == board[row + 2][col + 2]
-                == board[row + 3][col + 3]
-                != 0
-            ):
-                return True
-
-    # Check downward diagonals
-    for row in range(3, num_rows):
-        for col in range(num_cols - 3):
-            if (
-                board[row][col]
-                == board[row - 1][col + 1]
-                == board[row - 2][col + 2]
-                == board[row - 3][col + 3]
-                != 0
-            ):
-                return True
-
-    return False
-
-
-def play_game(board):
-    turn_counter = 0
-    game_over = False
-    player_turn = True
-
-    while not game_over:
-        if player_turn:
-            col = int(input("Player move:"))
-
-            if is_valid_col(board, col):
-                row = get_valid_row(board, col)
-                board = drop_token(board, row, col, 1)
+        while not game_over:
+            if player_turn:
+                try:
+                    col = int(input("Player move:"))
+                except ValueError as e:
+                    print("Not a valid move input, try again")
+                    continue
+                if self.is_valid_col(col):
+                    row = self.get_valid_row(col)
+                    self = self.drop_token(row, col, 1)
+                else:
+                    print("Not a valid move, try again")
+                    continue
             else:
-                print("Not a valid move, try again")
-                continue
-        else:
-            col = int(input("AI move:"))
+                try:
+                    col = int(input("AI move:"))
+                except ValueError as e:
+                    print("Not a valid move input, try again")
 
-            if is_valid_col(board, col):
-                row = get_valid_row(board, col)
-                board = drop_token(board, row, col, 2)
-            else:
-                print("Not a valid move, try again")
-                continue
+                if self.is_valid_col(col):
+                    row = self.get_valid_row(col)
+                    self = self.drop_token(row, col, 2)
+                else:
+                    print("Not a valid move, try again")
+                    continue
 
-        print_board(board)
-        if check_for_winner(board):
-            game_over = True
-            return -1 if player_turn else 10
-        if not get_valid_cols(board):
-            game_over = True
-            return 0
-        player_turn = not player_turn
-        turn_counter += 1
+            self.print()
+            if self.check_for_winner():
+                game_over = True
+                return -1 if player_turn else 10
+            if not self.legal_moves():
+                game_over = True
+                return 0
+            player_turn = not player_turn
+            turn_counter += 1
 
 
 if __name__ == "__main__":
-    board = setup_board(10, 6)
+    game_board = np.zeros((6, 4))
+
+    board = Board(game_board)
+    board.print()
+    board.play_game()
